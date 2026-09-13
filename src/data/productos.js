ad142d2b3937dfa4b951988b51ff668b4de4b3ae
michelmeneses <michelmeneses@users.noreply.github.com>
2026-09-13 09:08:29 -0500

    feat: agregar datos de productos del enunciado

---
+++ src/data/productos.js
@@ -0,0 +1,60 @@
+const productos = [
+    {
+      id: 1,
+      nombre: "Laptop Lenovo",
+      precio: 2800000,
+      stock: 8,
+      categoria: "Tecnología",
+    },
+    {
+      id: 2,
+      nombre: "Mouse Logitech",
+      precio: 120000,
+      stock: 25,
+      categoria: "Tecnología",
+    },
+    {
+      id: 3,
+      nombre: "Teclado Mecánico",
+      precio: 350000,
+      stock: 12,
+      categoria: "Tecnología",
+    },
+    {
+      id: 4,
+      nombre: "Silla Ergonómica",
+      precio: 850000,
+      stock: 5,
+      categoria: "Muebles",
+    },
+    {
+      id: 5,
+      nombre: "Escritorio",
+      precio: 1200000,
+      stock: 7,
+      categoria: "Muebles",
+    },
+    {
+      id: 6,
+      nombre: "Audífonos Sony",
+      precio: 450000,
+      stock: 18,
+      categoria: "Audio",
+    },
+    {
+      id: 7,
+      nombre: "Micrófono USB",
+      precio: 380000,
+      stock: 10,
+      categoria: "Audio",
+    },
+    {
+      id: 8,
+      nombre: "Monitor Samsung",
+      precio: 950000,
+      stock: 6,
+      categoria: "Tecnología",
+    },
+  ];
+  
+  module.exports = productos;
